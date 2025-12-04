36df58b9cf60f077918f4592e268d088ffe85d6b
cuogne <cuog.nc116@gmail.com>
2025-12-04 21:05:35 +0700

    init connect to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 const apiV1Routes = require('./routes/api.v1.routes');
+const connectMongoDB = require('./config/mongodb.config');
 
 const PORT = process.env.PORT || 3000;
+
+// connect to MongoDB
+connectMongoDB();
 
 app.use('/api/v1', apiV1Routes);
 
