1cf05a372cd153da7e0a56be7b9f75990525bc01
cuogne <cuog.nc116@gmail.com>
2025-12-16 15:05:48 +0700

    fix(booking model): seat

--- backend/src/models/booking.model.js
+++ backend/src/models/booking.model.js
@@ -5,12 +5,12 @@ const bookingSchema = new mongoose.Schema({
   showtime: { type: mongoose.Schema.Types.ObjectId, ref: 'Showtime', required: true, index: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
 
-  seat: {
+  seat: [{
     row: { type: String, required: true },
     number: { type: Number, required: true },
-  },
+  }],
 
-  price: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
 
   bookedAt: { type: Date, default: Date.now },
 });
